e336b5b5b2b672b88482481a6b0212b22bb66a3e
GhostAdults <ssy4240@sina.com>
2020-08-12 21:42:48 +0800

    update

--- deploy.bat
+++ deploy.bat
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 @echo off
 git add -A
 git commit -m update
 git push origin master
-=======
-@echo off
-git add -A
-git commit -m update
-git push origin master
->>>>>>> 9891533d6c4583413a35c1729be22abadc687e42
 exit
